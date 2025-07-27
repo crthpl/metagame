b48fb82ce62501ae536598eba7682ce00e00e6dc
Brian Smiley <34353764+briansmiley@users.noreply.github.com>
2025-07-27 10:10:57 -0700

    adds WTS site design credit (#54)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,26 +5,29 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="bg-dark-500 py-9 px-6">
-      <div className="container mx-auto flex gap-4 flex-col-reverse md:flex-row items-center justify-between">
-        <p className="text-cyan-300">© {year} Metagame. All rights reserved.</p>
-        <div className="flex space-x-4">
-          <Link
-            href={SOCIAL_LINKS.TWITTER}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </Link>
-          <Link
-            href={SOCIAL_LINKS.DISCORD}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Discord
-          </Link>
-        </div>
+    <footer className="bg-dark-500 py-9 px-6 flex flex-col gap-4">
+      <div className="flex justify-between gap-2">
+        <div className="container mx-auto flex gap-4 flex-col-reverse md:flex-row items-center justify-between">
+          <p className="text-cyan-300">© {year} Metagame. All rights reserved.</p>
+            <div className="flex space-x-4">
+              <Link
+                href={SOCIAL_LINKS.TWITTER}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </Link>
+              <Link
+                href={SOCIAL_LINKS.DISCORD}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Discord
+              </Link>
+            </div>
+          </div>
       </div>
+      <div className="text-center md:text-righ font-light text-sm">Website design taken from <Link href="https://wts.sh" target="_blank" rel="noopener noreferrer">WhatTheStack</Link>, by courteous permission of <Link href="https://darko.io" target="_blank" rel="noopener noreferrer">Darko</Link></div>
     </footer>
   );
 } 
