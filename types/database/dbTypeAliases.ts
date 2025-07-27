b227b9f050faf2af8c5e5f524f08c6a18e5db639
Brian Smiley <briantsmiley42@gmail.com>
2025-07-26 18:01:56 -0700

    fixes supabase types

--- types/database/dbTypeAliases.ts
+++ types/database/dbTypeAliases.ts
@@ -1,6 +1,8 @@
 import { Tables } from "@/types/database/supabase.types";
 
 export type DbSession = Tables<"sessions">;
+export type DbSessionView = Tables<"sessions_view">;
 export type DbLocation = Tables<"locations">;
 export type DbTicket = Tables<"tickets">;
+export type DbTicketView = Tables<"tickets_view">;
 export type DbProfile = Tables<"profiles">;
