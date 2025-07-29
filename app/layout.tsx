f32c381fc1bffcb6842a00dfd778d9c47090fd3b
Brian Smiley <briantsmiley42@gmail.com>
2025-07-28 18:49:05 -0700

    minor warning fix on viewport export on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Jura } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
@@ -50,10 +50,11 @@ export const metadata: Metadata = {
   icons: {
     icon: "/logo.svg",
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
