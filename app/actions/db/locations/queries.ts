58c142bcc2c756f1517fa1b4fcf8655862afe6aa
Brian Smiley <briantsmiley42@gmail.com>
2025-07-28 22:54:33 -0700

    adds some db server actions

--- app/actions/db/locations/queries.ts
+++ app/actions/db/locations/queries.ts
@@ -1,6 +1,8 @@
+'use server'
 import { createServiceClient } from "@/utils/supabase/service";
 
 export const getAllLocations = async () => {
+  console.log('getAllLocations')
   const supabase = createServiceClient()
   const { data, error } = await supabase
     .from('locations')
