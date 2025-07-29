12c5b5cbf0d28dd5002178c6392a26c08ed46c4c
Brian Smiley <briantsmiley42@gmail.com>
2025-07-28 22:54:00 -0700

    adds more db aliases

--- types/database/dbTypeAliases.ts
+++ types/database/dbTypeAliases.ts
@@ -1,8 +1,15 @@
 import { Tables } from "@/types/database/supabase.types";
 
 export type DbSession = Tables<"sessions">;
+export type DbSessionUpdate = Partial<DbSession>;
 export type DbSessionView = Tables<"sessions_view">;
 export type DbLocation = Tables<"locations">;
+export type DbLocationUpdate = Partial<DbLocation>;
 export type DbTicket = Tables<"tickets">;
+export type DbTicketUpdate = Partial<DbTicket>;
 export type DbTicketView = Tables<"tickets_view">;
 export type DbProfile = Tables<"profiles">;
+export type DbProfileUpdate = Partial<DbProfile>;
+export type DbSessionRsvp = Tables<"session_rsvps">;
+export type DbSessionRsvpUpdate = Partial<DbSessionRsvp>;
+export type DbSessionRsvpView = Tables<"session_rsvps_view">;
