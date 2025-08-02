6ce96edb74f7b5226e1a03cddc4744888525a841
Brian Smiley <briantsmiley42@gmail.com>
2025-08-02 01:26:07 -0400

    adds middleware hadnling for authed-only route like profile and logout

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,6 +1,9 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
+const loggedInOnlyRoutes = [
+  '/profile','/logout'
+]
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -40,14 +43,12 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    loggedInOnlyRoutes.some(route => request.nextUrl.pathname.startsWith(route))
   ) {
     // no user, potentially respond by redirecting the user to the login page
-    // const url = request.nextUrl.clone()
-    // TODO: Add any auth redirection we want here
-    // url.pathname = '/login'
-    // return NextResponse.redirect(url)
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
