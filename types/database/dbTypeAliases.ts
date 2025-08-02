c23c306f1425a7ef9964cd23caae3db28b5ce13b
Brian Smiley <briantsmiley42@gmail.com>
2025-08-02 04:06:29 -0400

    adds more aliasesfor db updates and inserts

--- types/database/dbTypeAliases.ts
+++ types/database/dbTypeAliases.ts
@@ -1,15 +1,24 @@
-import { Tables } from "@/types/database/supabase.types";
+import { Tables, TablesInsert, TablesUpdate } from "@/types/database/supabase.types";
 
 export type DbSession = Tables<"sessions">;
-export type DbSessionUpdate = Partial<DbSession>;
+export type DbSessionInsert = TablesInsert<"sessions">;
+export type DbSessionUpdate = TablesUpdate<"sessions">;
 export type DbSessionView = Tables<"sessions_view">;
+
 export type DbLocation = Tables<"locations">;
-export type DbLocationUpdate = Partial<DbLocation>;
+export type DbLocationInsert = TablesInsert<"locations">;
+export type DbLocationUpdate = TablesUpdate<"locations">;
+
 export type DbTicket = Tables<"tickets">;
-export type DbTicketUpdate = Partial<DbTicket>;
+export type DbTicketInsert = TablesInsert<"tickets">;
+export type DbTicketUpdate = TablesUpdate<"tickets">;
 export type DbTicketView = Tables<"tickets_view">;
+
 export type DbProfile = Tables<"profiles">;
-export type DbProfileUpdate = Partial<DbProfile>;
+export type DbProfileInsert = TablesInsert<"profiles">;
+export type DbProfileUpdate = TablesUpdate<"profiles">;
+
 export type DbSessionRsvp = Tables<"session_rsvps">;
-export type DbSessionRsvpUpdate = Partial<DbSessionRsvp>;
+export type DbSessionRsvpInsert = TablesInsert<"session_rsvps">;
+export type DbSessionRsvpUpdate = TablesUpdate<"session_rsvps">;
 export type DbSessionRsvpView = Tables<"session_rsvps_view">;
