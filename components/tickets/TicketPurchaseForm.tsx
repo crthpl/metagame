7b94e5943840b453d286d6f487f31bdd4d4e5e1b
Brian Smiley <briantsmiley42@gmail.com>
2025-08-05 15:38:33 -0400

    fixes env var

--- components/tickets/TicketPurchaseForm.tsx
+++ components/tickets/TicketPurchaseForm.tsx
@@ -9,7 +9,7 @@ import { SOCIAL_LINKS } from '../../config';
 const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
 if (!stripeKey) {
-  throw new Error('PUBLIC_STRIPE_PUBLISHABLE_KEY is not set');
+  throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not set');
 }
 
 const stripePromise = loadStripe(stripeKey);
--- lib/env.ts
+++ lib/env.ts
@@ -14,7 +14,7 @@ export const AIRTABLE_PAT = getEnvVar('AIRTABLE_PAT');
 export const AIRTABLE_BASE_ID = getEnvVar('AIRTABLE_BASE_ID');
 export const AIRTABLE_TABLE_NAME = getEnvVar('AIRTABLE_TABLE_NAME');
 export const STRIPE_SECRET_KEY = getEnvVar('STRIPE_SECRET_KEY');
-export const PUBLIC_STRIPE_PUBLISHABLE_KEY = getEnvVar('PUBLIC_STRIPE_PUBLISHABLE_KEY');
+export const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY = getEnvVar('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY');
 export const COUPONS = getEnvVar('COUPONS');
 
 // Helper function to get any environment variable
