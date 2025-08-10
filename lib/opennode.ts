7279302361bfe26c2ed102c287a493dbc73773a8
Brian Smiley <briantsmiley42@gmail.com>
2025-08-09 23:43:45 -0700

    adds getHostedUrl helped function for oepnnode

--- lib/opennode.ts
+++ lib/opennode.ts
@@ -51,3 +51,8 @@ export async function createChargeRaw(payload: OpenNodeChargeRequest): Promise<O
     : json;
   return data as OpenNodeCharge;
 }
+
+export function getHostedCheckoutUrl(orderId: string) {
+  const env = (process.env.NEXT_PUBLIC_OPENNODE_ENV || process.env.OPENNODE_ENV || 'dev') 
+  return `https://checkout${env === 'live' ? '' : '.dev'}.opennode.com/${orderId}`;
+}
