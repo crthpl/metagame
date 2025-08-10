6eadea8fb232bccb0e487c3db3d55fbcaad958d1
Brian Smiley <briantsmiley42@gmail.com>
2025-08-09 20:27:05 -0700

    updates tickets to show and flow with btc

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import SetAnimation from "../components/Set/SetAnimation";
-import { TicketCard } from "@/components/tickets/TicketCard";
+import Tickets from "@/components/tickets/Tickets";
 import Sponsors from "@/components/Sponsors";
 import { Hero } from "@/components/sections/home/Hero";
 import Calendar from "@/components/sections/home/Calendar";
@@ -29,23 +29,7 @@ export default function Home() {
           <div id="set-animation">
             <SetAnimation />
           </div>
-          <section className="mt-4 mb-8" id="tickets">
-            <div className="container mx-auto flex flex-col items-center">
-              <div className="mb-8">
-                <h2 className="text-center text-3xl font-bold">
-                  Grab a ticket!
-                </h2>
-              </div>
-              <div className="grid grid-cols-1 gap-8 pb-20 md:grid-cols-3">
-                <TicketCard ticketTypeId="player" />
-                <TicketCard ticketTypeId="dayPass" />
-                <TicketCard ticketTypeId="volunteer" />
-                <TicketCard ticketTypeId="student" />
-                <TicketCard ticketTypeId="financialAid" />
-                <TicketCard ticketTypeId="supporter" />
-              </div>
-            </div>
-          </section>
+          <Tickets />
         </div>
         <PacmanAnimation />
         <div className="container mx-auto px-4">
