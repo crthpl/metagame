c49dd48f279fc22ad6f2b430abb93a361c722a91
Brian Smiley <briantsmiley42@gmail.com>
2025-08-09 22:50:48 -0700

    adds nonrefundable disclaimer for btc

--- components/tickets/Tickets.tsx
+++ components/tickets/Tickets.tsx
@@ -19,19 +19,26 @@ export const Tickets: React.FC = () => {
           <h2 className="text-center text-3xl font-bold">Grab a ticket!</h2>
         </div>
 
-        <div className="mb-6 flex items-center gap-3">
-          <span className={`text-sm ${paymentMethod === 'usd' ? 'font-semibold' : 'opacity-70'}`}>USD</span>
-          <button
-            type="button"
-            onClick={togglePaymentMethod}
-            className="relative inline-flex h-8 w-16 items-center rounded-full bg-gray-700 transition-colors"
-            aria-label="Toggle payment method"
-          >
-            <span
-              className={`inline-block h-6 w-6 transform rounded-full bg-white transition-transform ml-1 ${paymentMethod === 'btc' ? 'translate-x-8' : ''}`}
-            />
-          </button>
-          <span className={`text-sm ${paymentMethod === 'btc' ? 'font-semibold' : 'opacity-70'}`}>BTC</span>
+        <div className="flex flex-col items-center mb-6">
+          <div className=" flex items-center gap-3">
+            <span className={`text-sm ${paymentMethod === 'usd' ? 'font-semibold' : 'opacity-70'}`}>USD</span>
+            <button
+              type="button"
+              onClick={togglePaymentMethod}
+              className="relative inline-flex h-8 w-16 items-center rounded-full bg-gray-700 transition-colors"
+              aria-label="Toggle payment method"
+            >
+              <span
+                className={`inline-block h-6 w-6 transform rounded-full bg-white transition-transform ml-1 ${paymentMethod === 'btc' ? 'translate-x-8' : ''}`}
+              />
+            </button>
+            <span className={`text-sm ${paymentMethod === 'btc' ? 'font-semibold' : 'opacity-70'}`}>BTC</span>
+          </div>
+          {paymentMethod === 'btc' && (
+            <span className="">
+              <span>*Bitcoin purchases are <span className="font-extrabold underline">non-refundable</span> for logistical reasons.</span>
+            </span>
+          )}
         </div>
 
         <div className="grid grid-cols-1 gap-8 pb-20 md:grid-cols-3">
