b2c7064c04c2bc9f85d828bbbabbbf992ffd6438
Brian Smiley <briantsmiley42@gmail.com>
2025-08-10 20:43:26 -0700

    updates ticketcode signup to check for existing user and sign up properly

--- app/actions/db/tickets.ts
+++ app/actions/db/tickets.ts
@@ -1,13 +1,31 @@
 'use server'
 import { ticketsService } from "@/lib/db/tickets";
 import { usersService } from "@/lib/db/users";
+import { createServiceClient } from "@/utils/supabase/service";
 
 export const signupByTicketCode = async ({email, password, ticketCode}: {email: string, password: string, ticketCode: string}) => {
     const ticket = await ticketsService.getTicketByCode({code: ticketCode});
     if (!ticket) {
         throw new Error("Ticket not found");
     }
-    const newUser = await usersService.createUser({email, password});
-    await ticketsService.updateTicketOwner({ticketCode, ownerId: newUser.user.id});
+    if (ticket.owner_id) {
+        throw new Error("Ticket already has an owner");
+    }
+    let userId: string;
+    const supabase = createServiceClient()
+    const existingUser = await usersService.getUserProfileByEmail({email})
+    if (existingUser) {
+        userId = existingUser.id;
+    } else {
+    const {data: {user}, error} = await supabase.auth.signUp({
+        email,
+        password,
+    })
+        if (!user || error) {
+            throw new Error("Error signing up new user: " + error?.message)
+        }
+        userId = user.id;
+    }
+    await ticketsService.updateTicketOwner({ticketCode, ownerId: userId});
     return ticket;
 }
