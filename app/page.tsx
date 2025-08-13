849e49e8e5e21f2041678a68909776cb899fad14
Brian Smiley <briantsmiley42@gmail.com>
2025-08-12 22:41:29 -0700

    componentizes schedulesection

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,7 @@ import Speakers from "@/components/sections/home/Speakers";
 import { ContactUs } from "@/components/sections/home/ContactUs";
 import HomePageWrapper from "@/components/HomePageWrapper";
 import PacmanAnimation from "@/components/PacmanAnimation";
-import Schedule from "./schedule/Schedule";
-import { Button } from "@/components/Button";
-import { CALL_FOR_SESSIONS } from "@/config";
+import ScheduleSection from "@/components/sections/home/ScheduleSection";
 
 export default function Home() {
   return (
@@ -20,21 +18,7 @@ export default function Home() {
           <Hero />
           <Calendar />
           <GetInvolved />
-          <section className="h-[calc(100vh-150px)] w-full max-w-full overflow-hidden flex flex-col justify-center mb-10">
-            <h2 className="text-center text-3xl font-bold mb-4">Schedule</h2>
-            <p className="text-center text-primary-200 mb-8">
-              Times, locations, content, hosts, and the fundamental fabric of reality all subject to change.
-            </p>
-            <div className="container mx-auto h-full flex flex-col border border-secondary-300 rounded-xl overflow-y-auto">
-              <Schedule
-              />
-            </div>
-            <div className="my-8 flex justify-center">
-              <Button background="bg-cyan-500" link={CALL_FOR_SESSIONS} target="_blank">
-                Submit a session proposal
-              </Button>
-            </div>
-          </section>
+          <ScheduleSection />
           <Speakers />
           <Sponsors />
           <div id="set-animation">
---
+++ components/sections/home/ScheduleSection.tsx
@@ -0,0 +1,23 @@
+import Schedule from "@/app/schedule/Schedule";
+import { Button } from "@/components/Button";
+import { CALL_FOR_SESSIONS } from "@/config";
+
+export default function ScheduleSection() {
+    return (
+        <section className="h-fit w-full max-w-full overflow-hidden flex flex-col justify-center mb-10">
+            <h2 className="text-center text-3xl font-bold mb-4">Schedule</h2>
+            <p className="text-center text-primary-200 mb-8">
+              Times, locations, content, hosts, and the fundamental fabric of reality all subject to change.
+            </p>
+            <div className="container mx-auto h-[calc(100vh-150px)] flex flex-col border border-secondary-300 rounded-xl overflow-y-auto">
+              <Schedule
+              />
+            </div>
+            <div className="my-8 flex justify-center">
+              <Button background="bg-cyan-500" link={CALL_FOR_SESSIONS} target="_blank">
+                Submit a session proposal
+              </Button>
+            </div>
+        </section>
+    )
+}
