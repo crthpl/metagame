cd625278b9fe1ce52b7fdbe8d110ab0317bc9bf2
Brian Smiley <briantsmiley42@gmail.com>
2025-08-12 18:57:22 -0700

    fixes externallinks on profile page lackjing protocol

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function toExternalLink(url: string) {
+  return url.startsWith('http') ? url : `https://${url}`;
+}
