c8ff486809cd13ed73e99f9137263064cc3cffb3
Brian Smiley <briantsmiley42@gmail.com>
2025-08-16 12:37:18 -0700

    cleans up some sponsor spacing

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -8,73 +8,79 @@ export default async function Sponsors() {
   const platinumSponsors = sponsors.filter((s) => s.tier === "platinum");
   const goldSponsors = sponsors.filter((s) => s.tier === "gold");
   const silverSponsors = sponsors.filter((s) => s.tier === "silver");
-
   return (
     <section className="mb-20 flex flex-col" id="sponsors">
       <div className="w-full flex flex-col items-center justify-center">
         <h2 className="mb-8 text-3xl font-bold">Sponsors</h2>
-        <h3 className="text-xl text-center font-bold mb-2 text-gray-400">Headline</h3>
-        <Image src="/logos/bitcoin-white.png" alt="Bitcoin" width={400} height={100}  />
-        {platinumSponsors.length > 0 && (
-          <div className="w-full mb-16 flex flex-col items-center">
-            <h3 className="text-2xl text-center font-bold mb-8 text-secondary-300">Platinum</h3>
-            <div className="grid grid-cols-1 gap-8 max-w-2xl">
-              {platinumSponsors
-                .sort((a, b) => a.id - b.id)
-                .map((sponsor) => (
-                  <PartnerCard
-                    key={sponsor.id}
-                    imgClass="h-32 sm:h-40 lg:h-48 w-auto max-w-full"
-                    partner={sponsor}
-                  />
-                ))}
-            </div>
+        <div className='flex flex-col items-center justify-center gap-12'>
+          {/* Headline */}
+          <div>
+            <h3 className="text-2xl text-center font-bold mb-2 text-white">Headline</h3>
+            {/* Bitcoin logo image size ratio is 1920/456 */}
+            <Image src="/logos/bitcoin-white.png" alt="Bitcoin" height={90} width={375} className='mt-6' />
           </div>
-        )}
-
-        {goldSponsors.length > 0 && (
-          <div className="w-full mb-12 flex flex-col items-center">
-            <h3 className="text-2xl text-center font-bold mb-8 text-yellow-400">Gold</h3>
-            <div className={`grid gap-12 ${
-              goldSponsors.length === 1 
-                ? 'grid-cols-1 max-w-3xl' 
-                : 'grid-cols-1 md:grid-cols-2 max-w-4xl'
-            }`}>
-              {goldSponsors
-                .sort((a, b) => a.id - b.id)
-                .map((sponsor) => (
-                  <PartnerCard
-                    key={sponsor.id}
-                    imgClass="h-48 w-full lg:w-auto lg:h-64 max-w-full"
-                    partner={sponsor}
-                  />
-                ))}
+          {/* Platinum */}
+          {platinumSponsors.length > 0 && (
+            <div className="w-full flex flex-col items-center">
+              <h3 className="text-2xl text-center font-bold mb-8 text-white">Platinum</h3>
+              <div className="grid grid-cols-1 gap-8 max-w-2xl">
+                {platinumSponsors
+                  .sort((a, b) => a.id - b.id)
+                  .map((sponsor) => (
+                    <PartnerCard
+                      key={sponsor.id}
+                      imgClass="h-32 sm:h-40 lg:h-48 w-auto max-w-full"
+                      partner={sponsor}
+                    />
+                  ))}
+              </div>
             </div>
-          </div>
-        )}
-
-        {silverSponsors.length > 0 && (
-          <div className="w-full flex flex-col items-center">
-            <h3 className="text-2xl text-center font-bold mb-2 text-gray-400">Silver</h3>
-            <div className={`grid md:gap-16 ${
-              silverSponsors.length === 1 
-                ? 'grid-cols-1 max-w-md' 
-                : silverSponsors.length === 2
-                ? 'grid-cols-1 md:grid-cols-2 max-w-4xl'
-                : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-5xl'
-            }`}>
-              {silverSponsors
-                .sort((a, b) => a.id - b.id)
-                .map((sponsor) => (
-                  <PartnerCard
-                    key={sponsor.id}
-                    imgClass="h-24 sm:h-40 md:h-48 lg:h-48 w-full"
-                    partner={sponsor}
-                  />
-                ))}
+          )}
+          {/* Gold */}
+          {goldSponsors.length > 0 && (
+            <div className="w-full flex flex-col items-center">
+              <h3 className="text-2xl text-center font-bold mb-8 text-yellow-400">Gold</h3>
+              <div className={`grid gap-12 ${
+                goldSponsors.length === 1
+                  ? 'grid-cols-1 max-w-3xl'
+                  : 'grid-cols-1 md:grid-cols-2 max-w-4xl'
+              }`}>
+                {goldSponsors
+                  .sort((a, b) => a.id - b.id)
+                  .map((sponsor) => (
+                    <PartnerCard
+                      key={sponsor.id}
+                      imgClass="h-48 w-full lg:w-auto lg:h-64 max-w-full"
+                      partner={sponsor}
+                    />
+                  ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+          {/* Silver */}
+          {silverSponsors.length > 0 && (
+            <div className="w-full flex flex-col items-center">
+              <h3 className="text-2xl text-center font-bold text-gray-300">Silver</h3>
+              <div className={`grid md:gap-16 ${
+                silverSponsors.length === 1
+                  ? 'grid-cols-1 max-w-md'
+                  : silverSponsors.length === 2
+                  ? 'grid-cols-1 md:grid-cols-2 max-w-4xl'
+                  : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-5xl'
+              }`}>
+                {silverSponsors
+                  .sort((a, b) => a.id - b.id)
+                  .map((sponsor) => (
+                    <PartnerCard
+                      key={sponsor.id}
+                      imgClass="h-24 sm:h-40 md:h-48 lg:h-48 w-full"
+                      partner={sponsor}
+                    />
+                  ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
