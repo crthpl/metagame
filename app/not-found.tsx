a0c67aa661f8a0322cca878f8544d3a2214c8d43
Brian Smiley <briantsmiley42@gmail.com>
2025-08-23 20:01:20 -0700

    adds a not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,9 @@
 export default function NotFound() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="text-lg">The page you are looking for does not exist.</p>
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="mt-4 text-lg">Sorry Mario!</p>
+      <p className="text-lg">Your page is in another castle 🏰</p>
     </div>
   )
 }
